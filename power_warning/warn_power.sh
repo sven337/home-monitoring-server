db322fa73da300fc9b2642494052fd218de461ee
sven337 <sven337@aquilenet.fr>
2019-11-06 20:02:59 +0100

    avoid multiple simultaneous calls, ADPS comes once every 2 sec

--- power_warning/warn_power.sh
+++ power_warning/warn_power.sh
@@ -1,6 +1,16 @@
-amixer sset PCM 255
-amixer sset Master 100
-amixer sset Speaker 100
-mpg123 alarm.mp3 &
-echo "POWER WARNING $1 W"| ~/sms-send-notification.sh
-echo "POWER WARNING $1 W" | mail -s 'POWER WARNING'  root
+amixer sset PCM 255 > /dev/null
+amixer sset Master 100 > /dev/null
+amixer sset Speaker 100 > /dev/null
+LAST_RUN_FILE=/tmp/.edf_adps_last_run_at
+if [ -f $LAST_RUN_FILE ]; then
+	if [ $(( ($(cat $LAST_RUN_FILE) + 10) < $(date +%s) )) -eq 0 ]; then
+		echo time out
+		exit 0
+	fi
+fi
+
+date +%s > $LAST_RUN_FILE
+
+mpg123 alarm.mp3 >/dev/null &
+echo "POWER WARNING ADPS "| ~/sms-send-notification.sh
+echo "POWER WARNING ADPS " | mail -s 'POWER WARNING'  root
