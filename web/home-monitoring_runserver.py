65e8a66c091990b3dacbadf5aa633045b9228287
Sven337 <sven337@aquilenet.fr>
2021-03-28 17:36:31 +0200

    use python not python2

--- web/home-monitoring_runserver.py
+++ web/home-monitoring_runserver.py
@@ -1,6 +1,6 @@
-#!/usr/bin/python2
+#!/usr/bin/python
 
 from home_monitoring_web import app
 import os
 os.environ['HOME_MONITORING_SERVER_SETTINGS'] = os.environ['HOME'] + '/.home-monitoring-server_settings'
-app.run(debug=True, threaded=True, host="192.168.1.6")
+app.run(debug=True, threaded=True, host="0.0.0.0")
