059bf446f687faed839aa06ef60079f2f57ec2ab
Sven337 <sven337@aquilenet.fr>
2023-05-19 20:33:28 +0200

    remove ledlamp

--- RF24bridge_server/rf24.c
+++ RF24bridge_server/rf24.c
@@ -1,98 +1,10 @@
 #include <stdio.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include "mqtt_login.h"
 
-#define PIPE_GAZ_ID 1
-#define PIPE_LEDLAMP_ID 2
-#define PIPE_MAILBOX_ID 3
+#define PIPE_LINKY_ID 3
 #define PIPE_THERMOMETER_ID 4
-
-static int ledlamp_reply(uint8_t *p)
-{
-	char buf[1000];
-	float temperature;
-#undef UNK
-#define UNK  printf("Unknown ledlamp reply %c %c %c %c\n", p[0], p[1], p[2], p[3]); return 1;
-	switch (p[0]) {
-		case 'T':
-			// thermal event
-			switch (p[1]) {
-				case 'E':
-					printf("Ledlamp thermal emergency, temp is %d\n", p[2] | p[3] << 8);
-					break;
-				case 'A':
-					printf("Ledlamp thermal alarm, temp is %d\n", p[2] | p[3] << 8);
-					break;
-				case '0':
-					printf("Ledlamp thermal stand down from alarm, temp is %d\n", p[2] | p[3] << 8);
-					break;
-				case 'N':
-					printf("Ledlamp thermal notify: temp is %d\n", p[2] | p[3] << 8);
-					break;
-				default:
-					UNK
-			};
-			break;
-		case 'R':
-			// remote command reply
-			switch (p[1]) {
-				case 'O':
-					printf("Ledlamp remote reply: lamp is off\n");
-					break;
-				case '1':
-					printf("Ledlamp remote reply: lamp is on, light level target %d\n", p[2] | p[3] << 8);
-					break;
-				default:
-					UNK
-			};
-			break;
-		case 'D':
-			// light duty cycle event
-			switch (p[1]) {
-				case 'I':
-					printf("Ledlamp increased power, duty cycle %d\n", p[2]);
-					break;
-				case 'D':
-					printf("Ledlamp decreased power, duty cycle %d\n", p[2]);
-					break;
-				case 'N':
-					printf("Ledlamp current duty cycle notify: %d\n", p[2]);
-					break;
-				default:
-					UNK
-			};
-			break;
-		case 'L':
-			// light level event
-			switch (p[1]) {
-				case 'N':
-					printf("Ledlamp current light level notify: %d\n", p[2]);
-					break;
-				default:
-					UNK
-			}
-			break;
-		case 'A':
-			// ambiant event
-			switch (p[1]) {
-				case 'N':
-
-					temperature = (p[2] << 8 | p[3]) / 100.0;
-					printf("Ledlamp ambient temperature %f\n", temperature);
-					sprintf(buf, "curl http://192.168.1.6:5000/update/temperature/officeAH/%f", temperature);
-					system(buf);
-					break;
-				default:
-					UNK
-			}
-			break;
-
-		default:
-			UNK
-	}
-
-	return 0;
-}
 
 static int therm_message(uint8_t *p)
 {
@@ -154,9 +66,6 @@ int handle_rf24_cmd(uint8_t pipe_id, uint8_t data[4])
 {
 	int error = 0;
 	switch (pipe_id) {
-		case PIPE_LEDLAMP_ID:
-			return ledlamp_reply(data);
-			break;
 		case PIPE_THERMOMETER_ID:
 			return therm_message(data);
 			break;
