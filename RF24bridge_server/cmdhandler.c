9b9688c54da63d6b51d8e37adac2edd1eb913f8d
Sven337 <sven337@aquilenet.fr>
2025-07-21 14:45:53 +0200

    rf24 kill direct linky serial code (no longer used)

--- RF24bridge_server/cmdhandler.c
+++ RF24bridge_server/cmdhandler.c
@@ -7,122 +7,6 @@
 #include "mqtt_login.h"
 extern int handle_rf24_cmd(uint8_t pipe_id, uint8_t data[4]);
 
-static int teleinfo_last_index = 0;
-
-// Accumulate PAPP over one minute then report to web server. Display it every minute too.
-static int teleinfo_accumulate_PAPP(int papp)
-{
-	static int cnt = 0;
-	static int accum = 0;
-
-	accum += papp;
-	cnt++;
-
-	if (cnt == 60) {
-		int val = accum / cnt;
-		char buf[2048];
-		sprintf(buf, "mosquitto_pub -h 192.168.1.6 -t 'edf/PAPP'  -u %s -P %s  -i teleinfo_updater -r -m '%d'", MQTT_USER, MQTT_PASS, val);
-		system(buf);
-		sprintf(buf, "mosquitto_pub -h 192.168.1.6 -t 'edf/index' -u %s -P %s  -i teleinfo_updater -r -m '%d'", MQTT_USER, MQTT_PASS, teleinfo_last_index);
-		system(buf);
-		accum = 0;
-		cnt = 0;
-		return true; // print
-	}
-
-	if (!(cnt % 15)) {
-		return true;
-	}
-	return false;
-}
-
-// Store the last value for teleinfo_accumulate_PAPP to report to web server. Display it every 5 minutes.
-static int teleinfo_filter_BASE(int base) 
-{
-	static int cnt = 0;
-
-	teleinfo_last_index = base / 1000;
-
-	if (!cnt) {
-		cnt++;
-		return 1; // print
-	}
-
-	cnt = (cnt + 1) % 300;
-	return 0;
-}
-
-static char teleinfo_cksum(const char *cmd)
-{
-	bool good;
-	int sum = 32;
-	int i;
-	for (i = 0; i < strlen(cmd); i++) {
-		if (cmd[i] == ' ') {
-			break;
-		}
-		sum += cmd[i];
-	}
-
-	i++;
-	for (; i < strlen(cmd); i++) {
-		if (cmd[i] == ' ') {
-			break;
-		}
-		sum += cmd[i];
-	}
-
-	sum = (sum & 63) + 32;
-	
-	char cksum = cmd[i+1];
-	good = (cksum == sum);
-
-	if (!good) {
-		fprintf(stderr, "teleinfo cksum error on %s\n", cmd);
-	}
-
-	return good;
-}
-
-#define STREQ(buf, s) !strncmp(buf, s, strlen(s))
-static void teleinfo_line(const char *cmd) 
-{
-	bool print = false;
-
-	if (STREQ(cmd, "ADCO ") ||
-		STREQ(cmd, "OPTARIF ") ||
-		STREQ(cmd, "ISOUSC ") ||
-		STREQ(cmd, "PTEC ") ||
-		STREQ(cmd, "IMAX ") ||
-		STREQ(cmd, "MOTDETAT ")) {
-		// Ignore these, they are not interesting
-		return;
-	}
-
-	if (!teleinfo_cksum(cmd)) {
-		return;
-	}
-
-	// In fact, the only interesting ones are ADPS, PAPP, and BASE
-
-	// Avertissement de Dépassement de Puissance Souscrite
-	if (STREQ(cmd, "ADPS ")) {
-		// warn everyone to turn things off because the breaker is going to blow
-		system("cd ../power_warning; ./warn_power.sh ''");
-		print = true;
-	} else if (STREQ(cmd, "PAPP ")) {
-		int papp = atoi(cmd + 5);
-		print = teleinfo_accumulate_PAPP(papp);
-	} else if (STREQ(cmd, "BASE ")) {
-		int base = atoi(cmd + 5);
-		print = teleinfo_filter_BASE(base);
-	}
-
-	if (print) {
-		printf("EDF %s\n", cmd);
-	}
-}
-
 static void rf24_line(const char *cmd)
 {
 	uint8_t data[4];
@@ -137,10 +21,7 @@ static void rf24_line(const char *cmd)
 
 void handle_serial_input_line(const char *cmd) 
 {
-	if (STREQ(cmd, "TELE ")) {
-		// Teleinfo command
-		teleinfo_line(cmd + 5);
-	} else if (STREQ(cmd, "RF24 ")) {
+	if (!strncmp(cmd, "RF24 ", 5)) {
 		rf24_line(cmd + 5);
 	} else {
 		printf("XXXfromserial \"%s\"\n", cmd);
